5439a34708d3acfbe5e862bb12c8d08f5494008e
Ko Juzz <sawdani@gmail.com>
2025-05-20 13:09:32 +0700

    [🚧 78] Setup App and Install Tailwindcss . . .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="bg-red-200">
+    Home Page
   </main>
 </template>
+
+<script setup></script>
+
+<style scoped></style>
